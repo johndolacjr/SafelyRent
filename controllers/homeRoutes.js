e5f8d9906453ec693b80a2b112451511bc3059a8
johndolac <johndolacjr@gmail.com>
2021-01-01 21:12:09 -0700

    updated CSS and other syntax throughout the code

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,7 +4,7 @@ const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
-    // Get all projects and JOIN with user data
+    // Get all tenant and JOIN with user data
     const tenantData = await Tenant.findAll({
       include: [
         {
